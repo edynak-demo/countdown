917d9be5caa8ccf1ae81f839f03ef3fa1e125e9d
edynak-demo <edynakdemo@gmail.com>
2020-04-13 11:50:08 -0400

    implemented scroll feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
+import { Element } from 'react-scroll';
 
 import Header from './components/topAndBottom/Header';
 import Footer from './components/topAndBottom/Footer';
@@ -12,17 +13,30 @@ import Location from './components/Location';
 class App extends Component {
   render() {
     return (
-      <div
-        className='App'
-        style={{ height: '1500px', background: 'cornflowerblue' }}
-      >
-        <Header />
-        <Featured />
-        <VenueInfo />
-        <Highlight />
-        <Pricing />
-        <Location />
-        <Footer />
+      <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
+        <Header/>
+
+        <Element name="featured">
+          <Featured/>
+        </Element>
+
+        <Element name="venueinfo">
+          <VenueInfo/>
+        </Element>
+        
+        <Element name="highlights">
+          <Highlight/>
+        </Element>
+        
+        <Element name="pricing">
+          <Pricing/>
+        </Element>
+
+        <Element name="location">
+          <Location/>
+        </Element>
+        
+        <Footer/>
       </div>
     );
   }
